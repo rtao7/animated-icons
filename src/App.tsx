7c024091ac5e0bf91c0b5656a3abd48f39952d1f
rayofsunshine <rtao1014@gmail.com>
2024-11-20 16:55:45 -0800

    landing page layout

--- src/App.tsx
+++ src/App.tsx
@@ -141,29 +141,29 @@ function Icon() {
   );
 }
 
-function Download() {
-  return (
-    <div>
-      <button className="focus-visible:outline-sky-50 m-auto flex items-center w-[180px] gap-2 text-sm px-4 py-4 bg-white border-1 border-black/10">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-file-down"
-        >
-          <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
-          <path d="M14 2v4a2 2 0 0 0 2 2h4" />
-          <path d="M12 18v-6" />
-          <path d="m9 15 3 3 3-3" />
-        </svg>
-        SVG
-      </button>
-    </div>
-  );
-}
+// function Download() {
+//   return (
+//     <div>
+//       <button className="focus-visible:outline-sky-50 m-auto flex items-center w-[180px] gap-2 text-sm px-4 py-4 bg-white border-1 border-black/10">
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           width="20"
+//           height="20"
+//           viewBox="0 0 24 24"
+//           fill="none"
+//           stroke="currentColor"
+//           stroke-width="2"
+//           stroke-linecap="round"
+//           stroke-linejoin="round"
+//           class="lucide lucide-file-down"
+//         >
+//           <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+//           <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+//           <path d="M12 18v-6" />
+//           <path d="m9 15 3 3 3-3" />
+//         </svg>
+//         SVG
+//       </button>
+//     </div>
+//   );
+// }
